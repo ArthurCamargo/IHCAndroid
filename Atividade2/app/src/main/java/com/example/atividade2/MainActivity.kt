5c583f8650b94ce80ba8943eec06195c4d860eed
guest <arthurcamargo13@gmail.com>
2022-02-21 22:37:20 -0300

    fix temperature and light

--- Atividade2/app/src/main/java/com/example/atividade2/MainActivity.kt
+++ Atividade2/app/src/main/java/com/example/atividade2/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.atividade13
+package com.example.atividade2
 
 import android.annotation.SuppressLint
 import android.content.Context
@@ -6,12 +6,14 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import com.example.atividade2.R
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 
 private lateinit var sensorManager : SensorManager
@@ -21,39 +23,47 @@ private lateinit var temperature : Sensor
 private lateinit var fusedLocation: FusedLocationProviderClient
 
 
+
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
 
 
-    private val light_view = findViewById<TextView>(R.id.light)
-    private val temperature_view = findViewById<TextView>(R.id.temperature)
-    private val latitude_view = findViewById<TextView>(R.id.latitude)
-    private val longitude_view = findViewById<TextView>(R.id.longitude)
-    private val altitude_view = findViewById<TextView>(R.id.altitude)
-    private val gps_button = findViewById<Button>(R.id.gps_button)
 
-
-    private var fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
+    private lateinit var lightView: TextView
+    private lateinit var temperatureView: TextView
+    private lateinit var latitudeView: TextView
+    private lateinit var longitudeView: TextView
+    private lateinit var altitudeView: TextView
+    private lateinit var gpsButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fusedLocation = LocationServices.getFusedLocationProviderClient(this)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
         temperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
 
-
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        lightView = findViewById<TextView>(R.id.light)
+        temperatureView = findViewById<TextView>(R.id.temperature)
+        latitudeView = findViewById<TextView>(R.id.latitude)
+        longitudeView = findViewById<TextView>(R.id.longitude)
+        altitudeView = findViewById<TextView>(R.id.altitude)
+        gpsButton = findViewById<Button>(R.id.gps_button)
+        gpsButton.setOnClickListener {
+            gps()
+        }
     }
 
     @SuppressLint("MissingPermission")
     fun gps() {
-        gps_button.setOnClickListener{
-            fusedLocationClient.lastLocation
+        gpsButton.setOnClickListener{
+            fusedLocation.lastLocation
                 .addOnSuccessListener{
-                    latitude_view.text = it.altitude.toString()
-                    longitude_view.text = it.longitude.toString()
-                    altitude_view.text = it.latitude.toString()
+                    latitudeView.text = it.altitude.toString()
+                    longitudeView.text = it.longitude.toString()
+                    altitudeView.text = it.latitude.toString()
                 }
         }
 
@@ -74,17 +84,17 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onSensorChanged(event: SensorEvent) {
         if(event.sensor.type == Sensor.TYPE_LIGHT)
         {
-            light_view.text = event.values[0].toString()
+            lightView.text = event.values[0].toString()
         }
 
         if(event.sensor.type == Sensor.TYPE_AMBIENT_TEMPERATURE)
         {
-            temperature_view.text = event.values[0].toString()
+           temperatureView.text = event.values[0].toString()
         }
 
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-
+        return
     }
 }
